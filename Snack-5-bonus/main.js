27ca8ae6fe438307490d4e36fe6774efb9067669
AntonioSabella <asabella6@gmail.com>
2022-04-01 15:45:55 +0200

    Add adjust to Snack-5-bonus

--- Snack-5-bonus/main.js
+++ Snack-5-bonus/main.js
@@ -2,7 +2,7 @@
  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro. */
 
 //Creo i due array
- let first_array = ['volpe','lupo','ghiottone','orso',];
+ let first_array = ['volpe','lupo','ghiottone','orso'];
  let second_array = ['lepre','cervo','usignolo','coniglio','scoiattolo','stambecco','procione','picchio'];
  console.log(first_array);
  console.log(second_array);
@@ -12,10 +12,12 @@ let go = true;
 while (go) {
     if (first_array.length < second_array.length) {
         first_array.push('falco');
-    }   else if (first_array.length == second_array.length) {
+    } else if (first_array.length > second_array.length) {
+        second_array.push('picchio');
+    } else if (first_array.length == second_array.length) {
         go = false
-    }  
+    }
 }
    
 console.log(first_array);
-console.log(second_array);
+console.log(second_array); 
