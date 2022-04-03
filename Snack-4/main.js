e0549d899b903ef4cae6d7474eee967a83af7f26
AntonioSabella <asabella6@gmail.com>
2022-04-03 19:45:15 +0200

    Add Snack-4 adjusts

--- Snack-4/main.js
+++ Snack-4/main.js
@@ -3,14 +3,24 @@
 let integers_array = [] 
 // Creazione di un ciclo for che inserisca, "scegliendo", numeri casuali nell'array
 for (let i = 0; i < 10; i++) {
-    let number_for_array = Math.ceil(Math.random() * 100);
+    let number_for_array = Math.floor(Math.random() * 100);
     console.log(number_for_array);
     integers_array.push(number_for_array);
 }
 
 console.log(integers_array);
 
-//console.log(integers_array[0]);
 let somma = 0;
-somma = integers_array[1] + integers_array[3] + integers_array[5] + integers_array[7] + integers_array[9]
+/* In questo modo limito la mia somma alle sole posizioni inserite da me
+    somma = integers_array[1] + integers_array[3] + integers_array[5] + integers_array[7] + integers_array[9]
+*/
+
+for (let i = 0; i < integers_array.length; i++) {
+    if (i % 2 != 0) {
+        somma += integers_array[i];
+    } else {
+        console.log('Il numero è in posizione pari');
+    }
+}
+
 console.log(somma);
